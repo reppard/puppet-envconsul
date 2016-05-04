511bc77c51e9e9866b40d956180d974442dd79cc
reppard <reppardwalker@gmail.com>
2016-05-04 18:50:59 -0400

    fix(params.pp): remove trailing slash

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class envconsul::params {
   $version      = '0.5.0'
   $platform     = 'linux'
   $arch         = 'amd64'
-  $releases_url = 'https://releases.hashicorp.com/envconsul/'
+  $releases_url = 'https://releases.hashicorp.com/envconsul'
   $base_name    = "envconsul_${version}"
   $extension    = ".zip"
 }
