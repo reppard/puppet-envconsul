45833e47ea9b41b93caf7bce44f4c94054be3a4e
Jonathan Niesen <jon.niesen@gmail.com>
2016-05-04 15:28:01 -0400

    refactor(*): add params class; use releases

--- manifests/fetch.pp
+++ manifests/fetch.pp
@@ -1,13 +1,13 @@
 # == Class: envconsul::fetch
 
 class envconsul::fetch (
-  $base_url  = 'https://github.com/hashicorp/envconsul/releases/download',
+  $base_url  = "${envconsul::releases_url}/${envconsul::version}",
   $base_name = "envconsul_${envconsul::version}_${envconsul::platform}_${envconsul::arch}",
-  $extension = '.tar.gz',
+  $extension = $envconsul::extension,
 ){
 
   $file_name = "${base_name}${extension}"
-  $file_url  = "${base_url}/v${envconsul::version}/${file_name}"
+  $file_url  = "${base_url}/${file_name}"
 
   wget::fetch { $file_url:
     destination => "/tmp/${file_name}",
