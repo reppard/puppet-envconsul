395d85e2aadafd13d80012e0e54cec1dda1798fa
reppard <reppardwalker@gmail.com>
2016-05-04 16:53:47 -0400

    test(init_spec.rb): add some specs for init

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 describe 'envconsul' do
+  let (:facts) {{
+    'kernel' => 'Linux'
+  }}
 
   context 'with defaults for all parameters' do
-    it { should contain_class('envconsul') }
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_class('envconsul') }
+    it { is_expected.to contain_class('envconsul::fetch') }
+    it { is_expected.to contain_class('envconsul::install') }
+    it { is_expected.to contain_class('envconsul::params') }
   end
 end
