098e8b070256c560035a30af6db4bbb9c153e79e
reppard <reppardwalker@gmail.com>
2016-05-04 16:01:11 -0400

    test(fetch_spec.rb): update spec'

--- spec/classes/fetch_spec.rb
+++ spec/classes/fetch_spec.rb
@@ -1,35 +1,20 @@
 require 'spec_helper'
 
 describe 'envconsul::fetch' do
-  context 'version <= 0.5.0' do
-    let(:params) do
-      { :arch => 'amd64', :platform => 'linux', :version => '0.5.0' }
-    end
-
-    let(:expected_url) do
-      [
-        'https://github.com/hashicorp/envconsul/releases/download',
-        'v0.5.0',
-        'envconsul_0.5.0_linux_amd64.tar.gz'
-      ].join("/")
-    end
-
-    it { should contain_wget__fetch('file_url').with_source(expected_url) }
+  let(:params) do
+    {
+      :base_url  => 'https://releases.hashicorp.com/envconsul/0.6.1',
+      :base_name => 'envconsul_0.6.1_linux_amd64',
+      :extension => '.zip'
+    }
   end
 
-  context 'version >= 0.6.0' do
-    let(:params) do
-      { :arch => 'amd64', :platform => 'linux', :version => '0.6.1' }
-    end
-
-    let(:expected_url) do
-      [
-        'https://github.com/hashicorp/envconsul/releases/download',
-        'v0.6.1',
-        'envconsul_0.6.1_linux_amd64.zip'
-      ].join("/")
-    end
-
-    it { should contain_wget__fetch('file_url').with_source(expected_url) }
+  let(:expected_url) do
+    [
+      'https://releases.hashicorp.com/envconsul/',
+      '0.6.1/envconsul_0.6.1_linux_amd64.zip'
+    ].join
   end
+
+  it { should contain_wget__fetch('file_url').with_source(expected_url) }
 end
