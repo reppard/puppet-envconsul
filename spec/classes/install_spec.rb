03d42ed23e3b9724f2f35e5f9ec45e0876005168
reppard <reppardwalker@gmail.com>
2016-05-04 20:07:39 -0400

    fix(install.pp): use unzip for zip

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe 'envconsul::install' do
+  let (:facts) {{
+    'kernel' => 'Linux'
+  }}
+
+  context 'zip file' do
+    let(:params) { { :file_name => 'file.zip' } }
+
+    it { is_expected.to contain_exec('unpack')
+          .with( :command => 'unzip /tmp/file.zip')
+    }
+  end
+
+  context 'tar.gz file' do
+    let(:params) { { :file_name => 'file.tar.gz' } }
+
+    it { is_expected.to contain_exec('unpack')
+          .with( :command => 'tar zxf /tmp/file.tar.gz --strip-components 1')
+    }
+  end
 end
