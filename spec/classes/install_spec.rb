9acffa2163e8c333b3691590badff2859fc9c02a
Patrick Aikens <patrick.aikens@manheim.com>
2017-06-08 09:33:48 -0400

    Fix tests to match new resource names

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -4,7 +4,7 @@ describe 'envconsul::install' do
   context 'zip file' do
     let(:params) { { :file_name => 'file.zip' } }
 
-    it { is_expected.to contain_exec('unpack')
+    it { is_expected.to contain_exec('envconsul_unpack')
           .with( :command => 'unzip -o /tmp/file.zip')
     }
   end
@@ -12,7 +12,7 @@ describe 'envconsul::install' do
   context 'tar.gz file' do
     let(:params) { { :file_name => 'file.tar.gz' } }
 
-    it { is_expected.to contain_exec('unpack')
+    it { is_expected.to contain_exec('envconsul_unpack')
           .with( :command => 'tar zxf /tmp/file.tar.gz --strip-components 1')
     }
   end
