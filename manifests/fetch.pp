ed0a56080158aaaa08946e8b389169208a9210a5
Patrick Aikens <patrick.aikens@manheim.com>
2017-06-08 08:31:38 -0400

    Add namespaces to resource names

--- manifests/fetch.pp
+++ manifests/fetch.pp
@@ -8,12 +8,12 @@ class envconsul::fetch (
   $file_name = "${base_name}${extension}"
   $file_url  = "${base_url}/${file_name}"
 
-  wget::fetch { 'file_url':
+  wget::fetch { 'envconsul_file_url':
     source      => $file_url,
     destination => "/tmp/${file_name}",
     timeout     => 600,
     verbose     => true,
     unless      => "test -x /usr/bin/envconsul && /usr/bin/envconsul --version 2>&1 | grep -q '${envconsul::version}'",
-    notify      => Exec['unpack'],
+    notify      => Exec['envconsul_unpack'],
   }
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -4,26 +4,26 @@ class envconsul::install (
   $file_name = $envconsul::fetch::file_name
 ){
   if $file_name =~ /^.*\.zip/ {
-    exec { 'unpack':
+    exec { 'envconsul_unpack':
       command     => "unzip -o /tmp/${file_name}",
       cwd         => '/usr/bin',
       path        => ['/bin', '/usr/bin', '/usr/local/bin'],
       refreshonly => true,
-      notify      => File['cleanup_file'],
+      notify      => File['envconsul_cleanup_file'],
       require     => Class['envconsul::fetch'],
     }
   } elsif $file_name =~ /^.*\.tar\.gz/ {
-    exec { 'unpack':
+    exec { 'envconsul_unpack':
       command     => "tar zxf /tmp/${file_name} --strip-components 1",
       cwd         => '/usr/bin',
       path        => ['/bin', '/usr/bin', '/usr/local/bin'],
       refreshonly => true,
-      notify      => File['cleanup_file'],
+      notify      => File['envconsul_cleanup_file'],
       require     => Class['envconsul::fetch'],
     }
   }
 
-  file { 'cleanup_file':
+  file { 'envconsul_cleanup_file':
     ensure => absent,
     backup => false,
     path   => "/tmp/${file_name}",
